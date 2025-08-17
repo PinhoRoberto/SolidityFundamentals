d6f09309feb11684fb1bf69932d85d7e2359d05f
PinhoRoberto <roberto.pinho@protonmail.com>
2025-08-17 17:23:22 -0300

    Add Basic Contract MytokenSol

---
+++ contracts/MyToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyTokenSol{
+    
+    string public name = "MyToken";
+    string public symbol = "MKT";
+    uint256 public totalSuply = 100000000;
+
+    mapping (address => uint256) public balances;
+
+    constructor (){
+        balances[msg.sender] = totalSuply;
+    }
+    function transfer (address to, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances [to] += amount;
+
+
+    }
+    }
