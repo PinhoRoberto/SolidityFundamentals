a682b64bb3076bfe716fc76047deaf2d38b11211
PinhoRoberto <roberto.pinho@protonmail.com>
2025-08-25 15:15:02 -0300

    add viewPure

---
+++ contracts/ViewPure.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract exViewPure{
+
+    uint public  myStorageVariable;
+
+    function getMyStorageVariable() public view returns(uint){
+        return myStorageVariable;
+    }
+
+    function getAddition(uint a, uint b)public pure returns(uint){
+        return a+b;
+    }
+
+    function setMyStorageVariable(uint _newVar) public {
+        myStorageVariable = _newVar;
+    }
+    
+    }
